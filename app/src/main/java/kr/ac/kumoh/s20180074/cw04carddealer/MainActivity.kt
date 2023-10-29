2b5237959e1e5e6a744de103875d4f8e81943410
thk <qkdrhks123@gmail.com>
2023-10-29 20:53:49 +0900

    w05 랜덤카드 - 1장 랜덤출력

--- app/src/main/java/kr/ac/kumoh/s20180074/cw04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180074/cw04carddealer/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.ac.kumoh.s20180074.cw04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20180074.cw04carddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(){
     private lateinit var main : ActivityMainBinding
@@ -11,10 +13,33 @@ class MainActivity : AppCompatActivity(){
         // TODO: 액티비티 초기화
         main = ActivityMainBinding.inflate(layoutInflater)
 
-        main.input.setOnClickListener {
-            main.card1.setImageResource(R.drawable.c_red_joker)
-        }
+        val c = Random.nextInt(52) // 52장 중 랜덤으로 뽑기
+        Log.i("뽑힌숫자", "${c}")
+        // getCardName() : 숫자에 해당하는 파일명 얻어오기
+        // getIdentifier() : 원하는 파일명, 찾고자 하는 res 내의 폴더명, 패키지명으로 해당 리소스의 id를 얻어오기
+        val res = resources.getIdentifier(getCardName(c), "drawable", packageName)
+
+        main.card1.setImageResource(res) // 해당 리소스로 이미지 설정하기
 
         setContentView(main.root)
+    }
+
+    fun getCardName(c: Int): String{
+        val shape = when(c / 13){
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        val number = when(c % 13){
+            0 -> "ace"
+            in 1..9 -> "${c % 13}"
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
 }
