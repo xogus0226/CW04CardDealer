2372863fb2c68489a766ab1c00934d227b6d123b
thk <qkdrhks123@gmail.com>
2023-10-29 18:04:48 +0900

    f2 배경색 지정/가로 화면 만들기

--- app/src/main/java/kr/ac/kumoh/s20180074/cw04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180074/cw04carddealer/MainActivity.kt
@@ -10,11 +10,13 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         // TODO: 액티비티 초기화
         main = ActivityMainBinding.inflate(layoutInflater)
-//        main.card1.setImageResource(R.drawable.c_red_joker)
-        setContentView(main.root)
 
-        main.input.setOnClickListener{
+        // 백그라운드 색상을 #060으로 설정
+
+        main.input.setOnClickListener {
             main.card1.setImageResource(R.drawable.c_red_joker)
         }
+
+        setContentView(main.root)
     }
 }
